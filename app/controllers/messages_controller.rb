83162f5d60e0ee42876ecec088a04dacd40a0322
stevezhere <sfang4@yahoo.com>
2016-10-07 13:51:41 -0500

    add user authentication to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /messages
   # GET /messages.json
   def index
